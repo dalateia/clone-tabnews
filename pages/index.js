290a134baf05f1f39b3474c5f2d8190390061045
Mateus Dalateia <88723050+dalateia@users.noreply.github.com>
2024-01-20 03:09:25 +0000

    Nova alteração da frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bem vindos ao TabNews, com mudanças</h1>
+    return <h1>Bem vindos ao TabNews, com mudanças e talvez algum erro. Beleza nenhum erro.</h1>
 }
 
 export default Home;
