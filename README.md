8ad3c175e40f5ccd092f60881e6a9adc4450f6db
Mateus Dalateia <88723050+dalateia@users.noreply.github.com>
2024-02-04 19:55:24 +0000

    adiciona scripts `link:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório inicial do curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Bruna, obrigado por cuidar de mim a apoiar meu crescimento, você é tudo pra mim!</h1>
+  return (
+    <h1>
+      Bruna, obrigado por cuidar de mim a apoiar meu crescimento, você é tudo
+      pra mim!
+    </h1>
+  );
 }
 
 export default Home;
